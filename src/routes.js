6fc7c1321af0c66c6aa6adcedcb9dce5a7fad8c4
figoperdana <192410102022@mail.unej.ac.id>
2022-01-21 11:12:26 +0700

    update

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const {
+  addBooksHandler,
+  getAllBookHandler,
+  getBookByIdHandler,
+  putBookByIdHandler,
+  deleteBookByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: putBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBookByIdHandler,
+  },
+];
+
+module.exports = routes;
